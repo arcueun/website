19dd6e85f10c30d6f341798d95706c9040fb61de
arcu-pc <arcueun@gmail.com>
2026-01-14 02:18:28 +1100

    refactor: move SOCIAL_LINKS into relevant component folder

--- src/components/SocialLink.jsx
+++ src/components/SocialLink.jsx
@@ -1,14 +1,30 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-function SocialLink({ href, icon }) {
+const SOCIAL_LINKS = [
+  { href: "https://github.com/arcueun", icon: faGithub },
+  { href: "mailto:arcueun@gmail.com", icon: faEnvelope },
+  { href: "https://www.linkedin.com/in/marcusbc/", icon: faLinkedin },
+  { href: "https://www.instagram.com/arcueun/", icon: faInstagram },
+];
+
+
+function SocialLink() {
     return (
-        <a 
-            href={href} 
-            target="_blank" 
-            rel="noopener noreferrer" 
-            className="social-icon">
-			<FontAwesomeIcon icon={icon} size="2x" />
-		</a>
+        <>
+          {SOCIAL_LINKS.map(({ href, icon }, index) => (
+            <a
+              key={index}
+              href={href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="social-icon"
+            >
+              <FontAwesomeIcon icon={icon} size="2x" />
+            </a>
+          ))}
+        </>
     );
 }
 
